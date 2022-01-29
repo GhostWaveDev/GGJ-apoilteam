ed40720f96dc784896293097e36c029c920a4d02
GhostWaveDev <81485696+GhostWaveDev@users.noreply.github.com>
2022-01-29 11:08:17 +0100

    Add files via upload

--- GGJ/scripts/slimerndm.gd
+++ GGJ/scripts/slimerndm.gd
@@ -1,20 +1,24 @@
-extends "res://scripts/enemy.gd"
+extends Enemy
 
 var compt = 0
+var jumpCooldown = 2
+var jumpTime = 0
+var theta
 
-func _ready():
-	speed = Vector2(10,10)
-#var angle = 0
-	compt = 0
+var dir = Vector2.ZERO
 
-func _process(delta):
-	
-	if compt < delta*1 :
-		position += speed
-	else :
-		position += Vector2(speed[0]*delta*cos(randf()*2*PI), speed[1]*delta*sin(randf()*2*PI))
+func _physics_process(delta):
+	if compt > jumpCooldown:
+		jumpTime = 1
 		compt = 0
+		theta = randf() * 2 * PI
+	
 	compt += delta
+	
+	if jumpTime > 0:
+		jumpTime -= delta
+		move_and_slide(Vector2(40 * cos(theta), 40 * sin(theta)))
+	
 	update()
 
 
